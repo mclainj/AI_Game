345ce01219f232aa6b51ed9ef477cf4cefb152ba
mclainj <mclain.asap@gmail.com>
2019-02-16 02:50:54 -0600

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera sceneCam;
+    public NavMeshAgent agent;
+    [SerializeField] GameObject target;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+
+
+
+
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = sceneCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }*/
+    }
+}
