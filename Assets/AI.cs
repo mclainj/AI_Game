6c8ae182a146c1f0f271000600e0033f7abbc4aa
mclainj <mclain.asap@gmail.com>
2019-02-16 22:29:16 -0600

    Nav Mesh and Player Controller implemented
    Nav mesh and AI class implemented. Distance calculator in AI class (may move to game manager class later). Also added basic player controller for x,z movement.

---
+++ Assets/AI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AI : MonoBehaviour
+{
+    [SerializeField] int distanceRoll = 5;
+    public NavMeshAgent agentAI;
+    public NavMeshAgent agentPlayer;
+    public GameObject finish;
+    public GameObject test;
+
+    private Transform lastPos;
+
+    bool moving = false;
+    private void Start()
+    {
+        //lastPos = agentAI.transform;
+        lastPos = test.transform;
+    }
+
+    private void Update()
+    {
+        //print("Dist between AI & Player: " + getPathDistance(agentAI, agentPlayer.transform));
+        //print("Dist between AI & target: " + getPathDistance(agentAI, finish.transform));
+        //print("TEST " + getPathDistance(agentAI, test.transform));
+        //print("Dist between Player & target: " + getPathDistance(agentPlayer, finish.transform));
+        Travel();
+
+    }
+
+    private void Travel()
+    {
+        if (!agentAI.hasPath)
+        {
+            agentAI.SetDestination(finish.transform.position);
+            moving = true;
+        }
+        else
+        {
+            //print(getPathDistance(agentAI, lastPos) + " / " + distanceRoll);
+            if (getPathDistance(agentAI, lastPos) >= distanceRoll)
+            {
+                //print("STOP");
+                agentAI.isStopped = true;
+                test.transform.position = agentAI.transform.position;
+                lastPos = test.transform;
+            }
+        }
+    }
+
+    private float getPathDistance(NavMeshAgent agent, Transform target) 
+    {
+        float distance = 0f;
+        NavMeshPath currPath = new NavMeshPath();
+        NavMesh.CalculatePath(agent.transform.position, target.position, agent.areaMask, currPath);
+        for(int i = 0; i < currPath.corners.Length - 1; i++)
+        {
+            distance += Vector3.Distance(currPath.corners[i], currPath.corners[i + 1]);
+        }
+
+        return distance; ;
+    }
+
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.CrossPlatformInput;
+
 public class PlayerController : MonoBehaviour
 {
 
     public Camera sceneCam;
     public NavMeshAgent agent;
     [SerializeField] GameObject target;
+
+    Rigidbody rigidbody;
+
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float xControlSpeed = 40f;
+    [Tooltip("In ms^-1")] [SerializeField] float zControlSpeed = 40f;
+    float xThrow, zThrow;
+
     // Update is called once per frame
 
     private void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
         //agent.SetDestination(target.transform.position);
     }
     void Update()
@@ -27,12 +39,25 @@ public class PlayerController : MonoBehaviour
                 agent.SetDestination(hit.point);
             }
         }
+        ProcessInput();
     }
 
+    private void ProcessInput()
+    {
+        ProcessTranslation();
+        // todo fix collision issue with enemy that causes player drift
+    }
 
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xControlSpeed * Time.deltaTime;
+        float xPos = transform.position.x + xOffset;
 
+        zThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float zOffset = zThrow * zControlSpeed * Time.deltaTime;
+        float zPos = transform.position.z + zOffset;
 
-
-
-
+        transform.position = new Vector3(xPos, transform.position.y, zPos);
+    }
 }
