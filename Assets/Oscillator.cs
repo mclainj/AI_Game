3aed6b6a1fc50ff135fe7eca6e8619e7f1eb71db
mclainj <mclain.asap@gmail.com>
2019-02-18 20:55:20 -0600

    Gates now carve into nav mesh
    Gates carve into nav mesh, making the nav mesh agent recalculate its path if it is obstructed

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    float movementFactor; // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set movement factor
+        // protect against period is zero
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; //grows continually from zero
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to 1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
